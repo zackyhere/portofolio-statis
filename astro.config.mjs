11fbebe45d47c36a9584aaa0c6877b3d5bd6e4ac
zackyhere <zackyy425@gmail.com>
2025-12-09 15:20:55 +0800

    pages fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    site: 'https://zackyhere.github.io/',
+    site: 'https://zackyhere.github.io/portofolio-statis',
     base: '/',
 });
