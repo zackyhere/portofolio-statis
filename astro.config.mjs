405f584e7f073a70d2655624087925c9f860947c
zackyhere <zackyy425@gmail.com>
2025-12-09 15:16:13 +0800

    update route

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    site: 'https://zackyhere.github.io/portofolio-statis',
-    base: '/portofolio-statis',
+    site: 'https://zackyhere.github.io/',
+    base: '/',
 });
