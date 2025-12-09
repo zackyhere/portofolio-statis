ad4d547b7b5568a2243f477974107560aa10572d
zackyhere <zackyy425@gmail.com>
2025-12-09 11:25:14 +0800

    first commit

---
+++ src/data/siteData.js
@@ -0,0 +1,13 @@
+export const navMenus = [
+    { name: 'Beranda', url: '/#' },
+    { name: 'Tentang', url: '#about' },
+    { name: 'Bisnis', url: '#business' },
+    { name: 'Projek', url: '#projects' },
+    { name: 'Keahlian', url: '#skills' },
+    { name: 'Github', url: '#github' },
+    { name: 'Certificates', url: '#certificates' },
+    { name: 'Testimoni', url: '#testimonials' },
+    { name: 'Kontak', url: '#contacts' }
+];
+
+export const siteTitle = 'Home'; // Default title
